53193650a30a2990e8c09428da84f1b50e0d917f
Elham Nejrabi <elhamnejrabi@Elhams-MacBook-Pro.local>
2025-09-28 12:58:08 -0700

    second

--- color.cpp
+++ color.cpp
@@ -14,5 +14,20 @@ public:
     Color() : red(0), green(0), blue(0) {}
     Color(int r, int g, int b) : red(r), green(g), blue(b) {}
 
-   
-}
+    void setRed(int r)   { red = r; }
+    void setGreen(int g) { green = g; }
+    void setBlue(int b)  { blue = b; }
+
+    // Getters
+    int getRed() const   { return red; }
+    int getGreen() const { return green; }
+    int getBlue() const  { return blue; }
+
+    // Print method
+    void print() const {
+        cout << left << setw(12) << "Color"
+             << " -> R: " << setw(3) << red
+             << " G: "   << setw(3) << green
+             << " B: "   << setw(3) << blue << endl;
+    }
+};
