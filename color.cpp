16add7be86930f82f2936c5204d442c650a4440a
Elham Nejrabi <elhamnejrabi@Elhams-MacBook-Pro.local>
2025-09-28 12:41:21 -0700

    first

---
+++ color.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+class Color {
+private:
+    int red;
+    int green;
+    int blue;
+
+public:
+   
+    Color() : red(0), green(0), blue(0) {}
+    Color(int r, int g, int b) : red(r), green(g), blue(b) {}
+
+   
+}
